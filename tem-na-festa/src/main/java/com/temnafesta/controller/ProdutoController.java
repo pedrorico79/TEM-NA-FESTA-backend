182b151933deee22e25826b04175569cda6e45d8
LauraBuzzato <laura.buzzato@sptech.school>
2026-04-03 17:14:01 -0300

    CRUD produto completo

--- tem-na-festa/src/main/java/com/temnafesta/controller/ProdutoController.java
+++ tem-na-festa/src/main/java/com/temnafesta/controller/ProdutoController.java
@@ -1,10 +1,66 @@
 package com.temnafesta.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.temnafesta.dto.produto.ProdutoRequestDto;
+import com.temnafesta.dto.produto.ProdutoResponseDto;
+import com.temnafesta.mapper.ProdutoMapper;
+import com.temnafesta.model.Produto;
+import com.temnafesta.service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDto> criar(@RequestBody @Valid ProdutoRequestDto dto) {
+
+        Produto produto = ProdutoMapper.toEntity(dto);
+        Produto criado = service.criar(produto);
+
+        return ResponseEntity.status(201)
+                .body(ProdutoMapper.toResponseDto(criado));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponseDto>> listar() {
+        return ResponseEntity.ok(
+                ProdutoMapper.toResponseDtoList(service.listar())
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDto> buscar(@PathVariable Integer id) {
+        return ResponseEntity.ok(
+                ProdutoMapper.toResponseDto(service.buscarPorId(id))
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDto> atualizar(
+            @PathVariable Integer id,
+            @RequestBody @Valid ProdutoRequestDto dto) {
+
+        Produto produto = ProdutoMapper.toEntity(dto);
+        Produto atualizado = service.atualizar(id, produto);
+
+        return ResponseEntity.ok(
+                ProdutoMapper.toResponseDto(atualizado)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- tem-na-festa/src/main/java/com/temnafesta/dto/produto/ProdutoRequestDto.java
+++ tem-na-festa/src/main/java/com/temnafesta/dto/produto/ProdutoRequestDto.java
@@ -1,4 +1,51 @@
 package com.temnafesta.dto.produto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public class ProdutoRequestDto {
+    @NotBlank
+    private String nome;
+
+    private String descricao;
+
+    @NotNull
+    @Positive
+    private Double precoVenda;
+
+    @NotNull
+    private Boolean isAtivo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(Double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public Boolean getAtivo() {
+        return isAtivo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        isAtivo = ativo;
+    }
 }
