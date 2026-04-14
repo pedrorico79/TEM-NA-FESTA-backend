bbe0707cd83a49f1bfbd8620e15efdcbf4109f9b
igorsimao-hub <igor.simao@sptech.school>
2026-04-13 22:04:14 -0300

    Criando entidade endereÃço

--- src/main/java/com/temnafesta/model/Endereco.java
+++ src/main/java/com/temnafesta/model/Endereco.java
@@ -9,5 +9,24 @@ public class Endereco {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(nullable = false, length = 8)
+    private String cep;
 
+    @Column(nullable = false, length = 150)
+    private String logradouro;
+
+    @Column(nullable = false, length = 10)
+    private String numero;
+
+    @Column(length = 100)
+    private String complemento;
+
+    @Column(nullable = false, length = 100)
+    private String bairro;
+
+    @Column(nullable = false, length = 100)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    private String estado;
 }
