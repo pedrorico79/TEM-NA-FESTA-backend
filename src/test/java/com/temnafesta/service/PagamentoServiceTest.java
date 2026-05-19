e0bda125977751df5e0bb6619b579a628e568b9c
pedrorico79 <pedro.rico@sptech.school>
2026-05-19 17:37:45 -0300

    feat(tests): add pasta de testes

---
+++ src/test/java/com/temnafesta/service/PagamentoServiceTest.java
@@ -0,0 +1,28 @@
+package com.temnafesta.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PagamentoServiceTest {
+
+    @Test
+    void criar() {
+    }
+
+    @Test
+    void buscarPorId() {
+    }
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void listarPorPedido() {
+    }
+
+    @Test
+    void deletar() {
+    }
+}
---
+++ src/test/java/com/temnafesta/service/UsuarioServiceTest.java
@@ -0,0 +1,44 @@
+package com.temnafesta.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioServiceTest {
+
+    @Test
+    void criar() {
+    }
+
+    @Test
+    void autenticar() {
+    }
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void listarAtivos() {
+    }
+
+    @Test
+    void buscarPorId() {
+    }
+
+    @Test
+    void desativar() {
+    }
+
+    @Test
+    void reativar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void atualizarSenha() {
+    }
+}
