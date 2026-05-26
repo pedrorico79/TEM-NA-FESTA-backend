dc65429d316705b62396a2c405e6f8dde781d432
igorsimao-hub <igor.simao@sptech.school>
2026-05-26 17:27:01 -0300

    refactor: refatora entidade de Campanha para Evento

---
+++ src/main/java/com/temnafesta/model/Evento.java
@@ -0,0 +1,42 @@
+package com.temnafesta.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "evento")
+public class Evento {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private LocalDate dataInicio;
+
+    @Column(nullable = false)
+    private LocalDate dataFim;
+
+    @Column(nullable = false, name = "is_ativa")
+    private Boolean ativa = true;
+
+    public Integer getId() {return id;}
+    public void setId(Integer id) {this.id = id;}
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public LocalDate getDataInicio() {return dataInicio;}
+    public void setDataInicio(LocalDate dataInicio) {this.dataInicio = dataInicio;}
+
+    public LocalDate getDataFim() {return dataFim;}
+    public void setDataFim(LocalDate dataFim) {this.dataFim = dataFim;}
+
+    public Boolean getAtiva() {return ativa;}
+    public void setAtiva(Boolean ativa) {this.ativa = ativa;}
+}
