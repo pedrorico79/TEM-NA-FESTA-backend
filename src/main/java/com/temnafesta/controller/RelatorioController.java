d4d3d1ffdb67d6318eb2dd5d00ae06391763c7a6
igorsimao-hub <igor.simao@sptech.school>
2026-06-07 15:50:13 -0300

    feat: cria endpoint de obterPedidosAgrupadosPorSemana

--- src/main/java/com/temnafesta/controller/RelatorioController.java
+++ src/main/java/com/temnafesta/controller/RelatorioController.java
@@ -1,7 +1,9 @@
 package com.temnafesta.controller;
 
 import com.temnafesta.dto.relatorio.kpi.KpiResponseDto;
+import com.temnafesta.dto.relatorio.pedidosporsemana.PedidosPorSemanaResponseDto;
 import com.temnafesta.service.RelatorioService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/relatorios")
@@ -21,11 +24,27 @@ public class RelatorioController {
         this.relatorioService = relatorioService;
     }
 
+    @Operation(summary = "Obtém os dados consumidos pela KPIs do relatório")
     @GetMapping("/kpis")
     public ResponseEntity<KpiResponseDto> obterKpis(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate de,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate ate
     ) {
         return ResponseEntity.ok(relatorioService.obterKpis(de, ate));
+    }
+
+
+    @Operation(summary = "Obtém a quantidade de pedidos agrupados por semana para o gráfico")
+    @GetMapping("/pedidos-por-semana")
+    public ResponseEntity<List<PedidosPorSemanaResponseDto>> obterPedidosAgrupadosPorSemana(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate de,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate ate
+    ){
+        List<PedidosPorSemanaResponseDto> dadosGrafico = relatorioService
+                .retornarPedidosPorSemana(de, ate);
+
+        if (dadosGrafico.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(dadosGrafico);
+
     }
 }
